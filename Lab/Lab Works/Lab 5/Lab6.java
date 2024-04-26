f57a3149a2151a12b84fabff3fe2f0566804d41e
Hibrul-Anam-Prantik <168169689+Hibrul-Anam-Prantik@users.noreply.github.com>
2024-04-26 19:22:51 +0600

    Add files via upload

---
+++ Lab/Lab Works/Lab 5/Lab6.java
@@ -0,0 +1,26 @@
+public class Lab6{
+  public static void main(String []args){
+    
+    int x= 3, y = 5;
+    System.out.println("Given ,\nx=3\ny=5\n");
+    
+    System.out.print("Before Post Increment,x= ");
+    System.out.println(x);
+    
+    System.out.print("Doint Post Increment,x= ");
+    System.out.println(x++);
+    
+    System.out.print("After Post Increment,x= ");
+    System.out.println(x);
+    System.out.println("===========================");
+    
+    System.out.print("Before Pre Increment,y= ");
+    System.out.println(y);
+    
+    System.out.print("Doint PRe Increment,y= ");
+    System.out.println(++y);
+    
+    System.out.print("After Pre Increment,y= ");
+    System.out.println(y);
+  }
+}
