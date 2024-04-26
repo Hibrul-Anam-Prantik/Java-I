9d09c42de5eb3d9ed9162b5beebf826b5a1b741a
Hibrul-Anam-Prantik <168169689+Hibrul-Anam-Prantik@users.noreply.github.com>
2024-04-26 18:30:14 +0600

    Create Array.java

---
+++ Basic Learning/Array/Array.java
@@ -0,0 +1,22 @@
+public class Array{
+  public static void main(String [] args){
+    
+    int [] num = new int[6];
+    
+    num[0] = 1;
+    num[1] = 2;
+    num[2] = 3;
+    num[3] = 4;
+    num[4] = 5;
+    num[5] = 6;
+    //int len = num.length;
+    System.out.println(num);
+    System.out.println("Array Size: "+num.length);
+    System.out.println("Num[0]: "+num[0]);
+    System.out.println("Num[1]: "+num[1]);
+    System.out.println("Num[2]: "+num[2]);
+    System.out.println("Num[3]: "+num[3]);
+    System.out.println("Num[4]: "+num[4]);
+    System.out.println("Num[5]: "+num[5]);
+  }
+}
