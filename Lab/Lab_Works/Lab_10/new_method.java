f2080cd50c14e1c7e8eb20e3be23e10737dfccc7
Hibrul-Anam-Prantik <hibrul.anam.prantik@g.bracu.ac.bd>
2024-05-13 12:02:46 +0600

    adder method

---
+++ Lab/Lab_Works/Lab_10/new_method.java
@@ -0,0 +1,20 @@
+package Lab.Lab_Works.Lab_10;
+import java.util.*;
+public class new_method
+{
+    public static void main(String [] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the value of \"x\" & \"y\" to calculate (x - y):\n__________________________\n");
+        System.out.print("x = ");
+        int x = sc.nextInt();
+        System.out.print("y = ");
+        int y = sc.nextInt();
+        System.out.println("__________________________\nResult:\n"+x+" - "+y+" = "+calc(x,y)+"(Ans)");
+        sc.close();
+    }
+    public static int calc(int a, int b)
+    {
+        return a - b;
+    }
+}
