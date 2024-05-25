e316a504e3db60371f0f602538c1d80606a1e458
Hibrul-Anam-Prantik <hibrul.anam.prantik@g.bracu.ac.bd>
2024-05-25 22:41:56 +0600

    task 7 ; from a array, find the prime and perfect numbers in two seperate methods and print them seperately with index

--- Lab/Lab_Assignments/A-10-Recursion_&_Final_review/Practise_Task_7_Prime_Perfect.java
+++ Lab/Lab_Assignments/A-10-Recursion_&_Final_review/Practise_Task_7_Prime_Perfect.java
@@ -1,7 +1,10 @@
 public class Practise_Task_7_Prime_Perfect {
     public static void main(String[] args) {
         int arr[] = { 6, 13, 28, 17, 3, 9, 11, 23, 10, 29, 12, 7 };
+        System.out.println("Prime Numbers:");
         prime(arr);
+        System.out.println();
+        System.out.println("Perfect Numbers:");
         perfect(arr);
     }
 
@@ -22,8 +25,14 @@ public class Practise_Task_7_Prime_Perfect {
 
     public static void perfect(int[] arr) {
         for (int i = 0; i < arr.length; i++) {
+            int sum = 0;
             for (int j = 1; j < arr[i]; j++) {
-
+                if (arr[i] % j == 0) {
+                    sum += j;
+                }
+            }
+            if (arr[i] == sum) {
+                System.out.println(i + ": " + arr[i]);
             }
         }
     }
