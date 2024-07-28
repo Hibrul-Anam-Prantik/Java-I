2289373c5fda9f76776e6f73409eeb90fe9847ed
Hibrul-Anam-Prantik <hibrul.anam.prantik@g.bracu.ac.bd>
2024-07-29 02:22:18 +0600

    Class work 2 added

---
+++ Tasks/Class_Works/Lab_7/ClassWork_2.java
@@ -0,0 +1,21 @@
+package Tasks.Class_Works.Lab_7;
+
+import java.util.Scanner;
+
+public class ClassWork_2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine().toLowerCase();
+        boolean palindrome = false;
+        int n = str.length();
+        for (int i = 0; i < n / 2; i++) {
+            if (str.charAt(i) == str.charAt(n - 1 - i)) {
+                palindrome = true;
+            } else {
+                palindrome = false;
+            }
+        }
+        System.out.println(palindrome);
+        sc.close();
+    }
+}
--- Tasks/Class_Works/Lab_7/Scanner.java
+++ Tasks/Class_Works/Lab_7/Scanner.java
@@ -1,5 +1,11 @@
 package Tasks.Class_Works.Lab_7;
 
+import java.io.InputStream;
+
 public class Scanner {
+
+    public Scanner(InputStream in) {
+        //TODO Auto-generated constructor stub
+    }
 
 }
