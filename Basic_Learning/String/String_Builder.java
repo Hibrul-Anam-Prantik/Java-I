9ebcce24d0119b5429c6630d8f865d1dfbe5e98d
Hibrul-Anam-Prantik <hibrul.anam.prantik@g.bracu.ac.bd>
2024-08-12 00:18:08 +0600

    String Compression

--- Basic_Learning/String/String_Builder.java
+++ Basic_Learning/String/String_Builder.java
@@ -4,8 +4,10 @@ public class String_Builder {
     public static void main(String[] args) {
         StringBuilder sb = new StringBuilder("A");
         System.out.println(sb);
-        // System.out.println(useAppend(sb.toString()));
+        System.out.println(useAppend(sb.toString()));
         useLoop(sb.toString());
+        String str = "aaabbccccaaaadddca";
+        System.out.println(compress(str));
     }
 
     public static String useAppend(String str) 
@@ -25,5 +27,22 @@ public class String_Builder {
             str += ch;
         }
         System.out.println(str);
+    }
+
+    public static String compress(String str) 
+    {
+        String newStr = "";
+        for (int i = 0; i < str.length(); i++) {
+            int count = 1;
+            while (i < str.length() - 1 && str.charAt(i) == str.charAt(i+1)) {
+                count ++;
+                i ++;
+            }
+            newStr += str.charAt(i);
+            if ( count > 1) {
+                newStr += count;
+            }
+        }
+        return newStr;
     }
 }
