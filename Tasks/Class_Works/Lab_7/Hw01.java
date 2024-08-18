0adf7156bf2d671ca50eb9784bfb931c92a4e361
Hibrul-Anam-Prantik <hibrul.anam.prantik@g.bracu.ac.bd>
2024-08-19 04:42:24 +0600

    Homework 1 added

---
+++ Tasks/Class_Works/Lab_7/Hw01.java
@@ -0,0 +1,31 @@
+package Tasks.Class_Works.Lab_7;
+import java.util.Scanner;
+
+public class Hw01 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        sc.close();
+        String newStr = "";
+        for(int i = str.length() - 1; i >= 0; i--) 
+        {
+            if (str.charAt(i) == ' ') {
+                for (int j = newStr.length() - 1; j >= 0 ; j--) 
+                {
+                    System.out.print(newStr.charAt(j));
+                }
+                System.out.print(" ");
+                newStr = "";
+            } 
+            if (str.charAt(i) != ' ') {  
+                newStr += str.charAt(i);
+            }
+            if(i == 0) {
+                for (int j = newStr.length() - 1; j >= 0 ; j--) 
+                {
+                    System.out.print(newStr.charAt(j));
+                }
+            }
+        }
+    }
+}
