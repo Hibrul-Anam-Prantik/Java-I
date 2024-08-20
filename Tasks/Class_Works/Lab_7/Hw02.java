e0cf55e265627b8d83ff9c63752568a1babb16ae
Hibrul-Anam-Prantik <hibrul.anam.prantik@g.bracu.ac.bd>
2024-08-20 18:19:34 +0600

    HomeWork 02 added

--- Tasks/Class_Works/Lab_7/Hw02.java
+++ Tasks/Class_Works/Lab_7/Hw02.java
@@ -11,7 +11,6 @@ public class Hw02
         sc.close();
         int count = 1;
         int i = 0;
-        // String newStr = "";
         while(i < str.length() - 1) 
         {
             if (str.charAt(i) == str.charAt(i + 1)) {
@@ -25,5 +24,24 @@ public class Hw02
             i ++;
         }
         System.out.print(""+str.charAt(str.length() - 1) + count);
-    }    
+        System.out.println();
+        // another way to conpress =>
+        compress(str);
+    } 
+    public static void compress(String str)
+    {
+        String newStr = ""; 
+        for (int i = 0; i < str.length(); i++)
+        {
+            int count = 1;
+            while (i < str.length() - 1 && str.charAt(i) == str.charAt(i+1)) 
+            {
+                count ++;
+                i ++;
+            }
+            newStr += str.charAt(i);
+            newStr += count;
+        }
+        System.out.print(newStr);
+    }   
 }
