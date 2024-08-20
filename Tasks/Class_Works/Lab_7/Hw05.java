9c6d63c0d4af7eaefada5f529b49eeb5e7e4b5bb
Hibrul-Anam-Prantik <hibrul.anam.prantik@g.bracu.ac.bd>
2024-08-20 22:30:16 +0600

    HomeWork 05 added

---
+++ Tasks/Class_Works/Lab_7/Hw05.java
@@ -0,0 +1,28 @@
+package Tasks.Class_Works.Lab_7;
+
+import java.util.Scanner;
+
+public class Hw05 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str1 = sc.nextLine();
+        String str2 = sc.nextLine();
+        sc.close();
+        for (int i = 0; i < str2.length(); i++) 
+        {
+            int j = 0;
+            String tempStr = "";
+            while(j < str1.length()) 
+            {
+                if (str1.toLowerCase().charAt(j) == str2.toLowerCase().charAt(i)) {
+                    tempStr += "";
+                } else {
+                    tempStr += str1.charAt(j);
+                }
+                j ++;
+            }
+            str1 = tempStr;
+        }
+        System.out.println(str1);
+    }
+}
