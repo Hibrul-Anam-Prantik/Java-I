910419b9848eec8497b2daede9f260e6f825c6aa
Hibrul-Anam-Prantik <hibrul.anam.prantik@g.bracu.ac.bd>
2024-08-20 18:51:08 +0600

    HomeWork 03 done

---
+++ Tasks/Class_Works/Lab_7/Hw03.java
@@ -0,0 +1,52 @@
+package Tasks.Class_Works.Lab_7;
+
+import java.util.Scanner;
+
+public class Hw03 {
+    public static void main(String [] args) 
+    {
+        Scanner sc = new Scanner (System.in);
+        String str1 = sc.nextLine().toLowerCase();
+        String str2 = sc.nextLine().toLowerCase();
+        sc.close();
+        String newStr = "";
+        // Searching the unique character from the 1st String
+        for (int i = 0; i < str1.length(); i++)
+        {
+            int count = 0;   // for unique character
+            int j = 0;
+            while (j < str2.length()) 
+            {
+                if (str2.charAt(j) == str1.charAt(i)) {  
+                    count ++;   // if common character found
+                    break;
+                }
+                j++;
+            }
+            if (count == 0) {          // checking if the character is unique
+                char ch = (char)(str1.charAt(i) - 32);    // adding the character's UpperCase to newStr
+                newStr += ch;
+                
+            }
+        }
+        // again same process for the 2nd String
+        for (int i = 0; i < str2.length(); i++)
+        {
+            int count = 0;
+            int j = 0;
+            while (j < str1.length()) 
+            {
+                if (str1.charAt(j) == str2.charAt(i)) {
+                    count ++;
+                    break;
+                }
+                j++;
+            }
+            if (count == 0) {
+                char ch = (char)(str2.charAt(i) - 32);    
+                newStr += ch;    
+            }
+        }
+        System.out.println(newStr);
+    }
+}
