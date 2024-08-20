12db0a68f2f80736af3e1548b397ae1c8fc5a0a7
Hibrul-Anam-Prantik <hibrul.anam.prantik@g.bracu.ac.bd>
2024-08-21 04:38:51 +0600

    Home Work 07 added

--- Tasks/Class_Works/Lab_7/Hw07.java
+++ Tasks/Class_Works/Lab_7/Hw07.java
@@ -1,8 +1,21 @@
 package Tasks.Class_Works.Lab_7;
 
+import java.util.Scanner;
+
 public class Hw07 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        
+        String str1 = sc.nextLine();
+        String str2 = sc.nextLine();
+        sc.close();
+        int count = 0;
+        for (int i = 0; i < str1.length() - 1; i++) 
+        {
+            String temp = "" +str1.charAt(i) + str1.charAt(i+1); 
+            if (temp.equalsIgnoreCase(str2)) {
+                count ++;
+            }
+        }
+        System.out.println("Occurrence: " + count + " times");
     }
 }
