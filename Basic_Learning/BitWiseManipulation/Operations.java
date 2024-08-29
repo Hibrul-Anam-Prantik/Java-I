654e7ac8a3559f6f15cfa18820ab5bf37680a917
Hibrul-Anam-Prantik <hibrul.anam.prantik@g.bracu.ac.bd>
2024-08-29 10:29:53 +0600

    Function for getting i-th bit

--- Basic_Learning/BitWiseManipulation/Operations.java
+++ Basic_Learning/BitWiseManipulation/Operations.java
@@ -1,12 +1,13 @@
 public class Operations {
     public static void main(String[] args) {
-        evenOdd(0);
-        evenOdd(1);
-        evenOdd(2);
-        evenOdd(3);
-        evenOdd(50);
-        evenOdd(99);
-        evenOdd(100);
+        // evenOdd(2);
+        // evenOdd(3);
+        int n = 10;
+        int i = 2;
+        System.out.println("The " + i + "-th bit of " + n + " is: " + get_ith_bit(n, i));
+        n = 15;
+        i = 3;
+        System.out.println("The " + i + "-th bit of " + n + " is: " + get_ith_bit(n, i));
     }
 
     public static void evenOdd (int num) {
@@ -16,13 +17,33 @@ public class Operations {
         } else { // ((num & bitmask) == 0)
             System.out.println(num + " is an Even Number.");
         }
+        /* Calculation;
+            * bitmask, 1    = 00000001
+            * any number, 5 = 00000101
+            * (5 & 1)       = 00000001 = 1
+            * any number, 6 = 00000110
+            * (5 & 1)       = 00000000 = 0
+        */
+    }
+
+    public static int get_ith_bit (int num, int i) {
+        int bitMask = 1 << i;
+        if ((num & bitMask) == 0) {
+            return 0;
+        } else {      // (num & bitMask) == 1)
+            return 1;
+        }
+        /* Calculation;
+            * Let, i = 2
+            * bitmask, 1 << i = 00000[1]00
+            * any number, 10  = 00001[0]10
+            * (10 & bitmask)  = 00000[0]00 = 0
+            * i = 3
+            * bitmask, 1 << i = 0000[1]000
+            * any number, 15  = 0000[1]111
+            * (15 & bitmask)  = 0000[1]000 = 1
+        */
     }
 }
 
-/* Calculation;
- * bitmask, 1    = 00000001
- * any number, 5 = 00000101
- * (5 & 1)       = 00000001 = 1
- * any number, 6 = 00000110
- * (5 & 1)       = 00000000 = 0
- */
+
