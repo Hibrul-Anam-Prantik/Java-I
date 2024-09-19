f60669381a60c2b1c2555f41ca4dcd1f4a5ca4c6
Hibrul-Anam-Prantik <hibrul.anam.prantik@g.bracu.ac.bd>
2024-09-19 18:52:28 +0600

    made a 2-D array

---
+++ Basic_Learning/Array/Array_2D/Array.java
@@ -0,0 +1,18 @@
+package Array_2D;
+
+public class Array {
+    public static void main(String[] args) {
+        int[][] arr = new int[3][5];
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[0].length; j++) {
+                arr[i][j] = i + j;
+            }
+        }
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[0].length; j++) {
+                System.out.print(arr[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
